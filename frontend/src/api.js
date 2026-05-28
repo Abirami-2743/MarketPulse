f89e4610c3bbdc28fc0bbc08e87f08a146bd7a97
Abirami-2743 <abiramisgp@gmail.com>
2026-05-28 08:17:20 +0530

    fix: connect frontend to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000'
+  baseURL: 'https://marketpulse-uols.onrender.com',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
 })
 
 export default api
